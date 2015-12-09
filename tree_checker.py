5b49f2244a5db78c450df1f9720e49bbba092588
Daniel <daniel.gonzalez@desy.de>
2015-12-09 11:26:06 +0100

    working version

--- tree_checker.py
+++ tree_checker.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
 import ROOT
+import sys
+from glob import glob
 
 def check_TreeExists(filename,treename):
      rootfile = ROOT.TFile.Open(filename)
      #print filename
      try:
           rootTree = rootfile.Get(treename)
-          #print 'True'
-          return True
+          entries = rootTree.GetEntriesFast()
+          #print filename,'True entries',entries,entries>0
+          return  entries>0
      except:
-          #print 'False'
+          print 'False'
           return False
      #entries = rootTree.GetEntriesFast()
      #if rootTree: return False
      #return True
+
+
+if __name__ == "__main__":
+     for arg in sys.argv[2:]:
+          if '*' in arg:
+               for itfile in glob(pattern):
+                     check_TreeExists(itfile,sys.argv[1])
+          else:
+               check_TreeExists(arg,sys.argv[1])
